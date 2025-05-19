6978ea3dc3f63a746006d0f874e2e17984b2a7f6
Luremento <zahar2005zima@gmail.com>
2025-05-19 17:23:38 +0800

    third_comm

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w-50 ">
+<div class="d-flex justify-content-center">
+    <h1>WorkHub</h1>
+</div>
+
+<div class="w-50 mx-auto">
     <form>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
+
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <input form-control type="date" id="date-input">
+        </div>
+        <h4>Выберите тип места </h4>
+        <div class="w-25 mb-3 d-flex flex-row justify-content-between ">
+
+            <input type="radio" value="one" class="btn-check" id="btn-checkOne" autocomplete="off">
+            <label class="btn btn-outline-primary" for="btn-checkOne">Рабочее</label><br>
+
+            <input type="radio" value="two" class="btn-check" id="btn-checkTwo" autocomplete="off">
+            <label class="btn btn-outline-primary" for="btn-checkTwo">Переговорное</label><br>
         </div>
         <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" id="exampleCheck1">
