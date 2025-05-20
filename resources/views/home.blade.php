c984094790b3ea04c64b8dfbc91bfbdcca8ba4bd
Luremento <zahar2005zima@gmail.com>
2025-05-20 19:08:18 +0800

    commitTHIRD

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,22 +8,27 @@
 <div class="w-50 mx-auto">
     <form>
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-
-        <div class="mb-3">
             <input form-control type="date" id="date-input">
         </div>
         <h4>Выберите тип места </h4>
         <div class="w-25 mb-3 d-flex flex-row justify-content-between ">
+            <input type="radio" value="one" name="type" class="btn-check" id="btn-checkOne" autocomplete="off">
+            <label class="btn btn-outline-primary button1" for="btn-checkOne">Рабочее</label><br>
 
-            <input type="radio" value="one" class="btn-check" id="btn-checkOne" autocomplete="off">
-            <label class="btn btn-outline-primary" for="btn-checkOne">Рабочее</label><br>
-
-            <input type="radio" value="two" class="btn-check" id="btn-checkTwo" autocomplete="off">
+            <input type="radio" value="two" onclick="addFn()" name="type" class="btn-check" id="btn-checkTwo" autocomplete="off">
             <label class="btn btn-outline-primary" for="btn-checkTwo">Переговорное</label><br>
+
+            <div class="qwe" style="display: none;">
+                <h1>элемент</h1>
+            </div>
+
+            <br>
+
+            <script>
+
+
+            </script>
+
         </div>
         <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -32,4 +37,12 @@
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
+
+<script>
+    const qwe = document.querySelector(".qwe")
+    const button1 = document.querySelector(".button1")
+    button1.addEventListener("click", () => {
+        qwe.style.display = "block"
+    })
+</script>
 @endsection
